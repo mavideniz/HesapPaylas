19ebb6e85cdf4d75c17308b78fc44b78fb794265
mavideniz <dogancanmavideniz@gmail.com>
2021-11-10 16:08:31 +0300

    file upload

---
+++ HesapPaylas/HesapPaylasApp.swift
@@ -0,0 +1,17 @@
+//
+//  HesapPaylasApp.swift
+//  HesapPaylas
+//
+//  Created by Doğancan Mavideniz on 6.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HesapPaylasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
